a6c0b5bdfc5c2c55739a162af3b6a138d7324279
Dell <Dell@ZISHAN>
2021-09-14 12:40:28 +0530

    Add project files.

---
+++ Vowels/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Vowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter The Charecter");
+            ch = Convert.ToChar(Console.ReadLine());
+            switch(ch)
+            {
+                case 'a':
+                case 'A': Console.WriteLine("It is a Vowel");
+                    break;
+                   
+                case 'i':
+                case 'I':
+
+                    Console.WriteLine("It is a Vowel");
+                    break;
+                case 'o':
+                case 'O':
+                    Console.WriteLine("It is a Vowel");
+                    break;
+                case 'u':
+                case 'U':
+                    Console.WriteLine("It is a Vowel");
+                    break;
+                default:
+                    Console.WriteLine("It is a Consonant");
+                    break;
+            }
+        }
+    }
+}
